0b35d6dbcbc615a964163b1a2358eea7f4763b28
Tatunori Obayashi <4210.tatu@gmail.com>
2018-04-04 22:44:09 +0000

    GETアクセス時にJSON形式のレスポンスを返す

---
+++ keigiban-api/index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+var server = http.createServer(getinfo);
+server.listen(8000);
+console.log('Server Start');
+
+//createServerの処理
+function getinfo(request,response){
+  var url_parts = url.parse(request.url,true);
+  switch (url_parts.pathname){
+    case '/index':
+    response_index(request,response);
+    break;
+    default:
+      response.writeHead(200,{'Content-Type':'text/html'});
+      response.end('no page...');
+      break;
+  }
+}
+
+function response_index(request,response){
+  if (request.method=="GET"){
+    var content = {
+      title:"タイトル",
+      id:"id",
+      msg:"メッセージ",
+    };
+    response.writeHead(200,{'Content-Type': 'application/json'});
+    response.write(JSON.stringify(content));
+    response.end();
+  }
+}
