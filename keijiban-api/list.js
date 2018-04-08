b9f44265c7ff2e533e0f944e5afca3fecc81e33b
Tatunori Obayashi <4210.tatu@gmail.com>
2018-04-08 22:05:40 +0000

    #17DB誤記を修正（kagigaya→kajigaya）

--- keijiban-api/list.js
+++ keijiban-api/list.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/keigiban',
+  'postgres://postgres:postgres@localhost/keijiban',
   { logging: false });
 const list = sequelize.define('lists', {
   id: {
